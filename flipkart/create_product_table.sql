083c91fd2a312531dd0c17b7476672b567b85afa
tony2020edx <64748043+tony2020edx@users.noreply.github.com>
2021-03-26 23:11:54 +0530

    Added a new column upc which is the unique product identifier

---
+++ flipkart/create_product_table.sql
@@ -0,0 +1,9 @@
+create table wash (Product_name Varchar(100) NOT NULL,
+brand Varchar(100) NOT NULL,
+ Sale_price DOUBLE,
+ mrp DOUBLE,
+ Discount_percentage INT,
+ Number_of_ratings DOUBLE,
+ Number_of_reviews DOUBLE,
+ Type_of_washing Varchar(100),
+ Product_url LONGTEXT);
---
+++ flipkart/test.sql
@@ -0,0 +1,9 @@
+create table wash (Product_name Varchar(100) NOT NULL,
+brand Varchar(100) NOT NULL,
+ Sale_price INT,
+ mrp INT,
+ Discount_percentage INT,
+ Number_of_ratings INT,
+ Number_of_reviews INT,
+ Type_of_washing Varchar(100),
+ Product_url LONGTEXT);
